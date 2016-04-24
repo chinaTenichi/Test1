bd726be3979c59d92462bff853f86a0838a98e2d
chinaTenichi <fatedomspp@hotmail.com>
2016-04-24 09:18:02 +0800

    第二次的提交

--- app/src/main/java/com/tenichi/test/MainActivity.java
+++ app/src/main/java/com/tenichi/test/MainActivity.java
@@ -4,10 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    public int getH() {
+        return h;
+    }
 
+    private int h;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
